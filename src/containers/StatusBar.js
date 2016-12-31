0ead97c858d6e183adb182b56bcbc45a5825b51f
kjirou <kjirou.web@gmail.com>
2016-12-31 16:08:58 +0900

    Refactor the reducer

--- src/containers/StatusBar.js
+++ src/containers/StatusBar.js
@@ -6,11 +6,11 @@ let StatusBar = ({ gameStatus }) => {
   return <div className="root__status-bar">
     <ul>
       <li>Progress: { gameStatus.progress } / { gameStatus.maxProgress }</li>
-      <li>TD: { gameStatus.technicalDebt } / { gameStatus.maxTechnicalDebt }</li>
     </ul>
   </div>;
 };
 
 StatusBar = connect(state => state)(StatusBar);
+
 
 module.exports = StatusBar;
--- src/reducers/game-status.js
+++ src/reducers/game-status.js
@@ -1,55 +1,37 @@
+const { createReducer } = require('redux-create-reducer');
+
 const { ACTION_TYPES, PARAMETERS } = require('../immutable/constants');
 
 
-const _createInitialState = () => {
+const createInitialState = () => {
   return {
     tickId: 0,
     maxProgress: PARAMETERS.MAX_PROGRESS,
-    maxTechnicalDebt: PARAMETERS.MAX_TECHNICAL_DEBT,
     progress: PARAMETERS.MIN_PROGRESS,
-    technicalDebt: PARAMETERS.MIN_TECHNICAL_DEBT,
   };
 };
+
 
 const alterLimitedValue = (value, delta, min, max) => {
   return Math.min(max, Math.max(min, value + delta));
 };
 
-const alterProgress = (state, { delta }) => {
-  return Object.assign({}, state, {
-    progress: alterLimitedValue(
-      state.progress, delta, PARAMETERS.MIN_PROGRESS, PARAMETERS.MAX_PROGRESS),
-  });
+
+const reducements = {
+  [ACTION_TYPES.ALTER_PROGRESS]: (state, { delta }) => {
+    return Object.assign({}, state, {
+      progress: alterLimitedValue(state.progress, delta, PARAMETERS.MIN_PROGRESS, PARAMETERS.MAX_PROGRESS),
+    });
+  },
+  [ACTION_TYPES.TICK]: (state) => {
+    return Object.assign({}, state, {
+      tickId: state.tickId + 1,
+    });
+  },
 };
 
-const alterTechnicalDebt = (state, { delta }) => {
-  return Object.assign({}, state, {
-    technicalDebt: alterLimitedValue(
-      state.technicalDebt, delta, PARAMETERS.MIN_TECHNICAL_DEBT, PARAMETERS.MAX_TECHNICAL_DEBT),
-  });
-};
-
-const tick = (state) => {
-  return Object.assign({}, state, {
-    tickId: state.tickId + 1,
-  });
-};
-
-const initialState = _createInitialState();
-const reduceGameStatus = (state = initialState, action) => {
-  switch (action.type || '') {
-    case ACTION_TYPES.ALTER_PROGRESS:
-      return alterProgress(state, action);
-    case ACTION_TYPES.ALTER_TECHNICAL_DEBT:
-      return alterTechnicalDebt(state, action);
-    case ACTION_TYPES.TICK:
-      return tick(state, action);
-    default:
-      return state;
-  }
-};
 
 module.exports = {
-  _createInitialState,
-  reduceGameStatus,
+  _createInitialState: createInitialState,
+  reduceGameStatus: createReducer(createInitialState(), reducements),
 };

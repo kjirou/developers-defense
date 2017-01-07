bd79c69dfa1ce432c2389f80688f5f566faaf5c7
kjirou <kjirou.web@gmail.com>
2017-01-07 11:14:16 +0900

    Make allies target to enemies

--- src/state-models/complex-apis.js
+++ src/state-models/complex-apis.js
@@ -1,11 +1,66 @@
 /** @module */
-const { BOARD_TYPES } = require('../immutable/constants');
-const { findSquareByCoordinate } = require('./square-matrix');
-const {
-  calculateActionPointsRecovery,
-  calculateMovementResults,
-} = require('./unit');
+const angles = require('angles');
+const boxCollide = require('box-collide');
 
+const { ACT_AIM_RANGE_TYPES, BOARD_TYPES, STYLES } = require('../immutable/constants');
+const { expandReachToRelativeCoordinates, matrixAdd } = require('../lib/core');
+const locationMethods = require('./location');
+const squareMatrixMethods = require('./square-matrix');
+const unitMethods = require('./unit');
+
+
+/**
+ * @param {State~Coordinate}
+ * @return {State~Location} A location of square
+ */
+const coordinateToSquareLocation = (coordinate) => {
+  return [coordinate[0] * STYLES.SQUARE_HEIGHT, coordinate[1] * STYLES.SQUARE_WIDTH];
+};
+
+/**
+ * @param {State~Location} squareLocation
+ * @return {{x, y, width, height}} This is mainly used to `substack/box-collide`
+ */
+const squareLocationToRect = (squareLocation) => {
+  return {
+    x: squareLocation[1],
+    y: squareLocation[0],
+    width: STYLES.SQUARE_WIDTH,
+    height: STYLES.SQUARE_HEIGHT,
+  };
+};
+
+/**
+ * @param {State~Coordinate} coordinate
+ */
+const coordinateToRect = (coordinate) => {
+  return squareLocationToRect(coordinateToSquareLocation(coordinate));
+};
+
+/**
+ * @param {State~Unit} unit
+ * @return {State~Location}
+ */
+const getUnitPositionAsLocation = (unit) => {
+  if (unit.location) {
+    return unit.location;
+  } else if (unit.placement) {
+    return coordinateToSquareLocation(unit.placement.coordinate);
+  }
+  throw new Error(`The unit does not have either \`location\` or \`placement\``);
+};
+
+/**
+ * @param {State~Location} centerSquareLocation
+ * @param {number} reach - A integer >= 0
+ * @return {{x, y, width, height}[]}
+ * @todo Remove overflowed coordinates from result?
+ */
+const createReachableRects = (centerSquareLocation, reach) => {
+  return expandReachToRelativeCoordinates(0, reach)
+    .map(relativeCoordinate => matrixAdd([centerSquareLocation], [coordinateToSquareLocation(relativeCoordinate)]))
+    .map(([location]) => squareLocationToRect(location));
+};
 
 /**
  * @return {State~Square[]}
@@ -16,7 +71,7 @@ const findSquaresFromBoardsByPlacement = (placement, ...boards) => {
   boards
     .filter(board => placement.boardType === board.boardType)
     .forEach(board => {
-      const square = findSquareByCoordinate(board.squareMatrix, placement.coordinate);
+      const square = squareMatrixMethods.findSquareByCoordinate(board.squareMatrix, placement.coordinate);
       if (square) squares.push(square);
     })
   ;
@@ -41,24 +96,131 @@ const findOneSquareFromBoardsByPlacement = (placement, ...boards) => {
 };
 
 /**
+ * @param {State~Unit} unit
+ * @return {boolean}
+ */
+const isUnitInBattle = (unit) => {
+  return unit.placement.boardType === BOARD_TYPES.BATTLE_BOARD && unit.hp > 0;
+};
+
+/**
+ * @param {State~Unit} actor - Only those in battle
+ * @param {Act} act
+ * @param {State~Unit} unit - Only those in battle
+ * @return {boolean}
+ */
+const willActorAimActAtUnit = (actor, act, unit) => {
+  return act.friendshipType === unitMethods.determineFriendship(actor, unit);
+};
+
+/**
+ * @param {State~Unit} actor - Only those in battle
+ * @param {Act} act
+ * @param {State~Unit} target - Only those in battle
+ * @return {boolean}
+ */
+const canActorAimActAtTargetedUnit = (actor, act, target) => {
+  if (act.aimRange.type === ACT_AIM_RANGE_TYPES.REACHABLE) {
+    // TODO: 暗黙的にユニットのサイズをマス目と同じとしているが、これで問題ないのか不明。
+    const actorLocation = getUnitPositionAsLocation(actor);
+    const reachableRects = createReachableRects(actorLocation, act.aimRange.reach);
+    const targetLocation = getUnitPositionAsLocation(target);
+    const targetRect = squareLocationToRect(targetLocation);
+    return reachableRects.some(rect => boxCollide(rect, targetRect));
+  }
+
+  throw new Error(`Invalid aim-range-type`);
+};
+
+/**
+ * @param {State~Unit} actor
+ * @param {Act} act
+ * @param {State~Unit[]} units
+ * @return {State~Unit[]}
+ */
+const choiceAimedUnit = (actor, act, units) => {
+  const aimableUnits = units
+    .filter(unit => willActorAimActAtUnit(actor, act, unit))
+    .filter(unit => canActorAimActAtTargetedUnit(actor, act, unit));
+
+  const actorLocation = getUnitPositionAsLocation(actor);
+  aimableUnits.sort((a, b) => {
+    const aLocation = getUnitPositionAsLocation(a);
+    const bLocation = getUnitPositionAsLocation(b);
+
+    // 1st: Sort by closest
+    const actorToA = locationMethods.measureDistance(actorLocation, aLocation);
+    const actorToB = locationMethods.measureDistance(actorLocation, bLocation);
+    if (actorToA < actorToB) {
+      return -1;
+    } else if (actorToA > actorToB) {
+      return 1;
+    }
+
+    // 2nd; Sort by clock-wise
+    const angleA = angles.fromSlope([-actorLocation[0], actorLocation[1]], [-aLocation[0], aLocation[1]]);
+    const angleB = angles.fromSlope([-actorLocation[0], actorLocation[1]], [-bLocation[0], bLocation[1]]);
+    if (angleA < angleB) {
+      return -1;
+    } else if (angleA > angleB) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  return aimableUnits[0] || null;
+};
+
+/**
  * Compute the state transition during the "tick"
  * <section>
  *   The "tick" is a coined word, which means so-called "one game loop".
  * </section>
- * @param {Object} state - A state generated from `store.getState()`
+ * @param {Object} state - A plain object generated from `store.getState()`
  * @return {Object}
  */
 const computeTick = ({ allies, enemies, gameStatus }) => {
-  const newAllies = allies.map(ally => {
-    const { actionPoints } = calculateActionPointsRecovery(ally);
+  let newAllies = allies.slice();
+  let newEnemies = enemies.slice();
 
-    return Object.assign({}, ally, {
-      actionPoints,
-    });
+  // TODO: 死亡者を盤上から除去するのは、ループの最初と最後どこで行う？または両方で？
+  //       想定ケース)
+  //       - 味方の手動退却 || 味方の死亡 -> 盤上から取り除かれてHP回復/出撃ゲージ空
+
+  // Ally's act
+  newAllies = newAllies.map(ally => {
+    const newAlly = Object.assign({}, ally);
+
+    if (!isUnitInBattle(newAlly)) {
+      // TODO: 出撃ポイントの回復
+      return newAlly;
+    }
+
+    const act = unitMethods.getAct(newAlly);
+    let didAct = false;
+
+    if (unitMethods.canDoAct(newAlly)) {
+      const aimedUnit = choiceAimedUnit(newAlly, act, newAllies.concat(newEnemies));
+      if (aimedUnit) {
+        // TODO: 効果を発生させる
+        // TODO: AP 消費
+        didAct = true;
+      }
+    }
+
+    if (!didAct) {
+      // Recover AP
+      const { actionPoints } = unitMethods.calculateActionPointsRecovery(newAlly);
+      Object.assign(newAlly, { actionPoints });
+    }
+
+    return newAlly;
   });
 
-  const newEnemies = enemies.map(enemy => {
-    const { location, destinationIndex } = calculateMovementResults(enemy);
+  // Enemy's act or movement
+  newEnemies = newEnemies.map(enemy => {
+    const { location, destinationIndex } = unitMethods.calculateMovementResults(enemy);
 
     return Object.assign({}, enemy, {
       location,
@@ -74,6 +236,12 @@ const computeTick = ({ allies, enemies, gameStatus }) => {
 
 
 module.exports = {
+  canActorAimActAtTargetedUnit,
+  choiceAimedUnit,
   computeTick,
+  coordinateToSquareLocation,
+  coordinateToRect,
+  createReachableRects,
   findOneSquareFromBoardsByPlacement,
+  willActorAimActAtUnit,
 };
--- test/state-models/complex-apis.js
+++ test/state-models/complex-apis.js
@@ -1,28 +1,132 @@
 const assert = require('power-assert');
 
-const { ACTION_TYPES, BOARD_TYPES } = require('../../src/immutable/constants');
+const { Act } = require('../../src/immutable/acts');
+const { ACT_AIM_RANGE_TYPES, ACTION_TYPES, BOARD_TYPES, FACTION_TYPES, FRIENDSHIP_TYPES
+  } = require('../../src/immutable/constants');
 const reducer = require('../../src/reducers');
-const { createNewBoardState } = require('../../src/state-models/board');
+const boardMethods = require('../../src/state-models/board');
+const coordinateMethods = require('../../src/state-models/coordinate');
+const locationMethods = require('../../src/state-models/location');
 const {
+  canActorAimActAtTargetedUnit,
+  choiceAimedUnit,
   computeTick,
+  coordinateToRect,
+  coordinateToSquareLocation,
+  createReachableRects,
   findOneSquareFromBoardsByPlacement,
+  willActorAimActAtUnit,
 } = require('../../src/state-models/complex-apis');
-const { createNewPlacementState } = require('../../src/state-models/placement');
+const placementMethods = require('../../src/state-models/placement');
+const unitMethods = require('../../src/state-models/unit');
 
 
 describe('state-models/complex-apis', () => {
+  const _createPlacedUnit = (rowIndex, columnIndex) => {
+    return Object.assign(unitMethods.createNewUnitState(), {
+      placement: Object.assign(placementMethods.createNewPlacementState(), {
+        boardType: BOARD_TYPES.BATTLE_BOARD,
+        coordinate: coordinateMethods.createNewCoordinateState(rowIndex, columnIndex),
+      }),
+    });
+  };
+
+  const _createPlacedAlly = (rowIndex, columnIndex) => {
+    return Object.assign(_createPlacedUnit(rowIndex, columnIndex), {
+      factionType: FACTION_TYPES.ALLY,
+    });
+  };
+
+  const _createLocatedUnit = (top, left) => {
+    return Object.assign(unitMethods.createNewUnitState(), {
+      location: locationMethods.createNewLocationState(top, left),
+    });
+  };
+
+  const _createNReachAct = (n, options = {}) => {
+    const friendshipType = options.friendshipType || null;
+
+    class NReachAct extends Act {}
+    return Object.assign(NReachAct, {
+      friendshipType,
+      aimRange: {
+        type: ACT_AIM_RANGE_TYPES.REACHABLE,
+        reach: n,
+      },
+    });
+  };
+
+
+  describe('coordinateToSquareLocation', () => {
+    it('can execute correctly', () => {
+      assert.deepStrictEqual(
+        coordinateToSquareLocation(coordinateMethods.createNewCoordinateState(0, 0)),
+        [0, 0]
+      );
+
+      assert.deepStrictEqual(
+        coordinateToSquareLocation(coordinateMethods.createNewCoordinateState(1, 2)),
+        [48, 96]
+      );
+    });
+  });
+
+  describe('coordinateToRect', () => {
+    it('can execute correctly', () => {
+      assert.deepStrictEqual(
+        coordinateToRect(coordinateMethods.createNewCoordinateState(0, 0)),
+        {
+          x: 0,
+          y: 0,
+          width: 48,
+          height: 48,
+        }
+      );
+
+      assert.deepStrictEqual(
+        coordinateToRect(coordinateMethods.createNewCoordinateState(1, 2)),
+        {
+          x: 96,
+          y: 48,
+          width: 48,
+          height: 48,
+        }
+      );
+    });
+  });
+
+  describe('createReachableRects', () => {
+    it('can execute correctly', () => {
+      assert.deepStrictEqual(createReachableRects([0, 0], 0), [
+        { x: 0, y: 0, width: 48, height: 48 },
+      ]);
+
+      assert.deepStrictEqual(createReachableRects([0, 0], 1), [
+        { x: 0, y: 0, width: 48, height: 48 },
+        { x: 0, y: -48, width: 48, height: 48 },
+        { x: 48, y: 0, width: 48, height: 48 },
+        { x: 0, y: 48, width: 48, height: 48 },
+        { x: -48, y: 0, width: 48, height: 48 },
+      ]);
+
+      assert.deepStrictEqual(createReachableRects([100, 150], 0), [
+        { x: 150, y: 100, width: 48, height: 48 },
+      ]);
+    });
+  });
+
   describe('findOneSquareFromBoardsByPlacement', () => {
     let sortieBoard;
     let battleBoard;
 
     beforeEach(() => {
-      sortieBoard = createNewBoardState(BOARD_TYPES.SORTIE_BOARD, 2, 3);
-      battleBoard = createNewBoardState(BOARD_TYPES.BATTLE_BOARD, 4, 5);
+      sortieBoard = boardMethods.createNewBoardState(BOARD_TYPES.SORTIE_BOARD, 2, 3);
+      battleBoard = boardMethods.createNewBoardState(BOARD_TYPES.BATTLE_BOARD, 4, 5);
     });
 
     it('can find a square from multiple boards by the placement interdisciplinary', () => {
       const s1 = findOneSquareFromBoardsByPlacement(
-        Object.assign(createNewPlacementState(), {
+        Object.assign(placementMethods.createNewPlacementState(), {
           boardType: BOARD_TYPES.SORTIE_BOARD,
           coordinate: [0, 0],
         }),
@@ -32,7 +136,7 @@ describe('state-models/complex-apis', () => {
       assert.strictEqual(s1, sortieBoard.squareMatrix[0][0]);
 
       const s2 = findOneSquareFromBoardsByPlacement(
-        Object.assign(createNewPlacementState(), {
+        Object.assign(placementMethods.createNewPlacementState(), {
           boardType: BOARD_TYPES.BATTLE_BOARD,
           coordinate: [3, 4],
         }),
@@ -44,7 +148,7 @@ describe('state-models/complex-apis', () => {
 
     it('should return a null if the placement is not exist', () => {
       const s1 = findOneSquareFromBoardsByPlacement(
-        Object.assign(createNewPlacementState(), {
+        Object.assign(placementMethods.createNewPlacementState(), {
           boardType: BOARD_TYPES.SORTIE_BOARD,
           coordinate: [0, 3],
         }),
@@ -54,7 +158,7 @@ describe('state-models/complex-apis', () => {
       assert.strictEqual(s1, null);
 
       const s2 = findOneSquareFromBoardsByPlacement(
-        Object.assign(createNewPlacementState()),
+        Object.assign(placementMethods.createNewPlacementState()),
         sortieBoard,
         battleBoard
       );
@@ -64,7 +168,7 @@ describe('state-models/complex-apis', () => {
     it('should throw a error if it find multiple squares', () => {
       assert.throws(() => {
         findOneSquareFromBoardsByPlacement(
-          Object.assign(createNewPlacementState(), {
+          Object.assign(placementMethods.createNewPlacementState(), {
             boardType: BOARD_TYPES.SORTIE_BOARD,
             coordinate: [0, 0],
           }),
@@ -72,6 +176,266 @@ describe('state-models/complex-apis', () => {
           sortieBoard
         );
       }, /SORTIE_BOARD/);
+    });
+  });
+
+  describe('willActorAimActAtUnit', () => {
+    class FriendlyAct extends Act {}
+    Object.assign(FriendlyAct, {
+      friendshipType: FRIENDSHIP_TYPES.FRIENDLY,
+    });
+
+    class UnfriendlyAct extends Act {}
+    Object.assign(UnfriendlyAct, {
+      friendshipType: FRIENDSHIP_TYPES.UNFRIENDLY,
+    });
+
+    let ally1;
+    let ally2;
+    let enemy1;
+    let enemy2;
+
+    beforeEach(() => {
+      ally1 = unitMethods.createNewAllyState();
+      ally2 = unitMethods.createNewAllyState();
+      enemy1 = unitMethods.createNewEnemyState();
+      enemy2 = unitMethods.createNewEnemyState();
+    });
+
+    it('should return true in the case of `ally -(friendly act)-> ally`', () => {
+      assert.strictEqual(willActorAimActAtUnit(ally1, FriendlyAct, ally2), true);
+    });
+
+    it('should return false in the case of `ally -(friendly act)-> enemy`', () => {
+      assert.strictEqual(willActorAimActAtUnit(ally1, FriendlyAct, enemy1), false);
+    });
+
+    it('should return false in the case of `enemy -(friendly act)-> ally`', () => {
+      assert.strictEqual(willActorAimActAtUnit(enemy1, FriendlyAct, ally1), false);
+    });
+
+    it('should return true in the case of `enemy -(friendly act)-> enemy`', () => {
+      assert.strictEqual(willActorAimActAtUnit(enemy1, FriendlyAct, enemy2), true);
+    });
+
+    it('should return false in the case of `ally -(unfriendly act)-> ally`', () => {
+      assert.strictEqual(willActorAimActAtUnit(ally1, UnfriendlyAct, ally2), false);
+    });
+
+    it('should return true in the case of `ally -(unfriendly act)-> enemy`', () => {
+      assert.strictEqual(willActorAimActAtUnit(ally1, UnfriendlyAct, enemy1), true);
+    });
+
+    it('should return true in the case of `enemy -(unfriendly act)-> ally`', () => {
+      assert.strictEqual(willActorAimActAtUnit(enemy1, UnfriendlyAct, ally1), true);
+    });
+
+    it('should return false in the case of `enemy -(unfriendly act)-> enemy`', () => {
+      assert.strictEqual(willActorAimActAtUnit(enemy1, UnfriendlyAct, enemy2), false);
+    });
+  });
+
+  describe('canActorAimActAtUnit', () => {
+    context('REACHABLE type', () => {
+      const ZeroReachAct = _createNReachAct(0);
+      const OneReachAct = _createNReachAct(1);
+      const TwoReachAct = _createNReachAct(2);
+
+      describe('coordinate base calculation', () => {
+        it('can check by 0 reach', () => {
+          const actor = _createPlacedUnit(2, 3);
+
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, ZeroReachAct, _createPlacedUnit(2, 3)), true);
+
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, ZeroReachAct, _createPlacedUnit(1, 3)), false);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, ZeroReachAct, _createPlacedUnit(3, 3)), false);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, ZeroReachAct, _createPlacedUnit(2, 2)), false);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, ZeroReachAct, _createPlacedUnit(2, 4)), false);
+        });
+
+        it('can check by 1 reach', () => {
+          const actor = _createPlacedUnit(2, 3);
+
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, OneReachAct, _createPlacedUnit(2, 3)), true);
+
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, OneReachAct, _createPlacedUnit(1, 3)), true);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, OneReachAct, _createPlacedUnit(3, 3)), true);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, OneReachAct, _createPlacedUnit(2, 2)), true);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, OneReachAct, _createPlacedUnit(2, 4)), true);
+
+          // [-2, 0], [+2, 0], [0, -2], [0, +2]
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, OneReachAct, _createPlacedUnit(0, 3)), false);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, OneReachAct, _createPlacedUnit(4, 3)), false);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, OneReachAct, _createPlacedUnit(2, 1)), false);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, OneReachAct, _createPlacedUnit(2, 5)), false);
+
+          // [-1, -1], [-1, +1], [+1, -1], [+1, +1]
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, OneReachAct, _createPlacedUnit(1, 2)), false);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, OneReachAct, _createPlacedUnit(1, 4)), false);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, OneReachAct, _createPlacedUnit(3, 2)), false);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, OneReachAct, _createPlacedUnit(3, 4)), false);
+        });
+
+        it('can check by 2 reach', () => {
+          const actor = _createPlacedUnit(2, 3);
+
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, TwoReachAct, _createPlacedUnit(2, 3)), true);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, TwoReachAct, _createPlacedUnit(3, 3)), true);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, TwoReachAct, _createPlacedUnit(4, 3)), true);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, TwoReachAct, _createPlacedUnit(5, 3)), false);
+        });
+      });
+
+      describe('location base calculation', () => {
+        it('can check by 0 reach', () => {
+          const actor = _createLocatedUnit(100, 150);
+
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, ZeroReachAct, _createLocatedUnit(100, 150)), true);
+
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, ZeroReachAct, _createLocatedUnit(53, 150)), true);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, ZeroReachAct, _createLocatedUnit(100, 197)), true);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, ZeroReachAct, _createLocatedUnit(147, 150)), true);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, ZeroReachAct, _createLocatedUnit(100, 103)), true);
+
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, ZeroReachAct, _createLocatedUnit(52, 150)), false);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, ZeroReachAct, _createLocatedUnit(100, 198)), false);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, ZeroReachAct, _createLocatedUnit(148, 150)), false);
+          assert.strictEqual(canActorAimActAtTargetedUnit(actor, ZeroReachAct, _createLocatedUnit(100, 102)), false);
+        });
+      });
+    });
+  });
+
+  describe('choiceAimedUnit', () => {
+    const ZeroReachAct = _createNReachAct(0, { friendshipType: FRIENDSHIP_TYPES.FRIENDLY });
+    const OneReachAct = _createNReachAct(1, { friendshipType: FRIENDSHIP_TYPES.FRIENDLY });
+    const TwoReachAct = _createNReachAct(2, { friendshipType: FRIENDSHIP_TYPES.FRIENDLY });
+
+    describe('reach check', () => {
+      it('should aim the reach=0 act to same place', () => {
+        //
+        //  012
+        // 0-T-
+        // 1TTT
+        // 2-T-
+        //
+        const targets = [
+          _createPlacedAlly(0, 1),
+          _createPlacedAlly(1, 0),
+          _createPlacedAlly(1, 1),
+          _createPlacedAlly(1, 2),
+          _createPlacedAlly(2, 1),
+        ];
+
+        assert.strictEqual(choiceAimedUnit(_createPlacedAlly(1, 1), ZeroReachAct, targets), targets[2]);
+        assert.strictEqual(choiceAimedUnit(_createPlacedAlly(2, 2), ZeroReachAct, targets), null);
+      });
+
+      it('should aim the reach=1 act to adjacent place', () => {
+        //
+        //  012
+        // 0T--
+        // 1---
+        // 2--T
+        //
+        const targets = [
+          _createPlacedAlly(0, 0),
+          _createPlacedAlly(2, 2),
+        ];
+
+        assert.strictEqual(choiceAimedUnit(_createPlacedAlly(0, 1), OneReachAct, targets), targets[0]);
+        assert.strictEqual(choiceAimedUnit(_createPlacedAlly(1, 0), OneReachAct, targets), targets[0]);
+        assert.strictEqual(choiceAimedUnit(_createPlacedAlly(1, 2), OneReachAct, targets), targets[1]);
+        assert.strictEqual(choiceAimedUnit(_createPlacedAlly(2, 1), OneReachAct, targets), targets[1]);
+        assert.strictEqual(choiceAimedUnit(_createPlacedAlly(1, 1), OneReachAct, targets), null);
+      });
+
+      it('should aim the reach=2 act to distant place', () => {
+        //
+        //  012
+        // 0T--
+        // 1---
+        // 2---
+        //
+        const targets = [
+          _createPlacedAlly(0, 0),
+        ];
+
+        assert.strictEqual(choiceAimedUnit(_createPlacedAlly(0, 2), TwoReachAct, targets), targets[0]);
+        assert.strictEqual(choiceAimedUnit(_createPlacedAlly(1, 1), TwoReachAct, targets), targets[0]);
+        assert.strictEqual(choiceAimedUnit(_createPlacedAlly(2, 0), TwoReachAct, targets), targets[0]);
+        assert.strictEqual(choiceAimedUnit(_createPlacedAlly(1, 2), TwoReachAct, targets), null);
+        assert.strictEqual(choiceAimedUnit(_createPlacedAlly(2, 1), TwoReachAct, targets), null);
+      });
+    });
+
+    describe('closest order', () => {
+      it('can execute correctly', () => {
+        //
+        //  012
+        // 0T--
+        // 1TT-
+        // 2---
+        //
+        const targets = [
+          _createPlacedAlly(0, 0),
+          _createPlacedAlly(1, 0),
+          _createPlacedAlly(1, 1),
+        ];
+
+        assert.strictEqual(choiceAimedUnit(_createPlacedAlly(2, 0), TwoReachAct, targets), targets[1]);
+        assert.strictEqual(choiceAimedUnit(_createPlacedAlly(1, 2), TwoReachAct, targets), targets[2]);
+      });
+    });
+
+    describe('clock-wise order', () => {
+      it('should give priority to the top', () => {
+        //
+        //  012
+        // 0-T-
+        // 1T-T
+        // 2-T-
+        //
+        const targets = [
+          _createPlacedAlly(2, 1),
+          _createPlacedAlly(0, 1),
+          _createPlacedAlly(1, 2),
+          _createPlacedAlly(1, 0),
+        ];
+
+        assert.strictEqual(choiceAimedUnit(_createPlacedAlly(1, 1), OneReachAct, targets), targets[1]);
+      });
+
+      it('should give priority to the right', () => {
+        //
+        //  012
+        // 0---
+        // 1T-T
+        // 2-T-
+        //
+        const targets = [
+          _createPlacedAlly(1, 0),
+          _createPlacedAlly(2, 1),
+          _createPlacedAlly(1, 2),
+        ];
+
+        assert.strictEqual(choiceAimedUnit(_createPlacedAlly(1, 1), OneReachAct, targets), targets[2]);
+      });
+
+      it('should give priority to the down', () => {
+        //
+        //  012
+        // 0---
+        // 1T--
+        // 2-T-
+        //
+        const targets = [
+          _createPlacedAlly(2, 1),
+          _createPlacedAlly(1, 0),
+        ];
+
+        assert.strictEqual(choiceAimedUnit(_createPlacedAlly(1, 1), OneReachAct, targets), targets[0]);
+      });
     });
   });
 

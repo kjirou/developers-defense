69cb409e8358e2326bcda7f4e84082cbd357f2ec
kjirou <kjirou.web@gmail.com>
2017-01-07 15:49:22 +0900

    Fix a comment

--- src/state-models/location.js
+++ src/state-models/location.js
@@ -1,7 +1,7 @@
 /**
  * @typedef {Object} State~Location
  * @description A position on the battle-board
- * @property {number} y - A distance from top to bottom in other word. In other words in CSS term is "top".
+ * @property {number} y - A distance from top to bottom. In other words in CSS terms is "top".
  * @property {number} x - A distance from left to right. In other words in CSS terms is "left".
  */
 

54002df1e612eb6bfcec8c30e85df5da9b25e2bf
kjirou <kjirou.web@gmail.com>
2017-02-04 23:15:46 +0900

    Add a test

--- test/components/SquareMatrix.js
+++ test/components/SquareMatrix.js
@@ -1,6 +1,8 @@
 const { mount, shallow } = require('enzyme');
+const jsdom = require('jsdom').jsdom;
 const assert = require('power-assert');
 const React = require('react');
+const sinon = require('sinon');
 
 const Square = require('../../src/components/Square');
 const SquareMatrix = require('../../src/components/SquareMatrix');
@@ -10,11 +12,52 @@ const { createNewSquareMatrixState } = require('../../src/state-models/square-ma
 describe('components/SquareMatrix', () => {
   const _matrix = createNewSquareMatrixState;
 
+  beforeEach(() => {
+    global.document = jsdom();
+    global.window = document.defaultView;
+  });
+
+  afterEach(() => {
+    delete global.window;
+    delete global.document;
+  });
+
   describe('constructor', () => {
     it('can create element', () => {
       const squareMatrix = _matrix(2, 3);
-      const wrapper = shallow(React.createElement(SquareMatrix, { squareMatrix }));
+      const wrapper = mount(React.createElement(SquareMatrix, { squareMatrix }));
       assert.strictEqual(wrapper.find(Square).length, 6);
+    });
+  });
+
+  describe('handleTouchStartPad', done => {
+    let mocks;
+
+    beforeEach(() => {
+      mocks = [];
+    });
+
+    afterEach(() => {
+      mocks.forEach(mock => mock.restore());
+    });
+
+    it('should be emitted on "touchstart"', done => {
+      const mock = sinon.stub(SquareMatrix, '_normalizeTouchPositions', () => {
+        return {};
+      });
+      mocks.push(mock);
+
+      const squareMatrix = _matrix(2, 3);
+      const handleTouchStartPad = () => {
+        done();
+      };
+
+      const wrapper = mount(React.createElement(SquareMatrix, { squareMatrix, handleTouchStartPad }));
+      const touchpadWrapper = wrapper.find('.square-matrix__touchpad');
+
+      assert.strictEqual(touchpadWrapper.length, 1);
+
+      touchpadWrapper.simulate('touchstart');
     });
   });
 });
